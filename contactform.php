1e08678cb287e822847bca318f011937e53c4764
Cory Dawson <cdd59@georgetown.edu>
2019-03-18 12:16:39 -0400

    added mail functions to populate final response message to my email

--- contactform.php
+++ contactform.php
@@ -3,7 +3,14 @@
 if (isset($_POST['submit'])) {
 	$name = $_POST ['name'];
 	$subject = $_POST ['subject'];
-	$mailfrom = $_POST ['mail'];
+	$mailFrom = $_POST ['mail'];
 	$message = $_POST ['message'];
+
+	$mailTo = "cdd59@georgetown.edu"
+	$headers = "From: Cory's Website"
+	$txt = "You have a new email from" .$name. ".\n\n".$message;
+
+
+	mail($mailto, $subject, $txt);
 
 }
