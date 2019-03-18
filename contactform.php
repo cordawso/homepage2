027fdb96e0f4c81b3311b70db986d24b4993cbc7
Cory Dawson <cdd59@georgetown.edu>
2019-03-18 10:46:14 -0400

    finish specifying variables

--- contactform.php
+++ contactform.php
@@ -2,8 +2,8 @@
 
 if (isset($_POST['submit'])) {
 	$name = $_POST ['name'];
-	$name = $_POST ['name'];
-	$name = $_POST ['name'];
-	$name = $_POST ['name'];
+	$subject = $_POST ['subject'];
+	$mailfrom = $_POST ['mail'];
+	$message = $_POST ['message'];
 
 }
