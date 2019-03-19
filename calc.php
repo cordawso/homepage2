40ff74f038efed219697e9f83ef9db549a591a04
Cory Dawson <cdd59@georgetown.edu>
2019-03-18 20:52:49 -0400

    add add operation and echo out the answers

--- calc.php
+++ calc.php
@@ -4,5 +4,10 @@ $num1 = $_POST ["num1"];
 $num2 = $_POST ["num2"];
 $operation = $_POST ["operations"];
 
-
+if ($operation == "add") 
+{
+	$answer_add = num1 + num2;
+	echo num1." + ".num2." = ";
+	echo $answer_add;
+}
 ?>
